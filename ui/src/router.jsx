83d42883fc0877370c800c0e8d7a6cd0850b0368
Linus Wahlgren <linus.wahlgren3@gmail.com>
2024-06-24 11:51:04 +0200

    Added a amcharts tree 2

--- ui/src/router.jsx
+++ ui/src/router.jsx
@@ -3,6 +3,7 @@ import { Text } from "@chakra-ui/react"
 import App from "./App"
 import Explore from "./Pages/Explore"
 import Tree from "./Pages/Tree"
+import Tree2 from "./Pages/Tree2"
 
 const NotFound = () => <Text>Page not found</Text>
 
@@ -11,6 +12,7 @@ const router = createBrowserRouter(
         <Route path="/" element={<App />}>
             <Route index element={<Explore />} />
             <Route path="tree" element={<Tree />} />
+            <Route path="tree2" element={<Tree2 />} />
             <Route path="*" element={<NotFound />} />
         </Route>
     ), { basename: import.meta.env.BASE_URL }
